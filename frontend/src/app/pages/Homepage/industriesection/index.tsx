414167e88e62437eece0ddcec5c3144dea0e0026
charu.maheshwari <yash.gupta@anarish.com>
2026-04-18 19:02:31 +0530

    Added school erp section

--- frontend/src/app/pages/Homepage/industriesection/index.tsx
+++ frontend/src/app/pages/Homepage/industriesection/index.tsx
@@ -5,14 +5,13 @@ export default function IndustriesSection() {
   return (
     <section className="py-12 md:py-24 bg-white overflow-hidden">
       <div>
-        <div className="relative w-full  rounded-3xl overflow-hidden shadow-2xl border border-gray-100 transition-all duration-500 hover:shadow-[0_20px_50px_rgba(0,0,0,0.1)]">
+        <div className="relative w-full  rounded-3xl overflow-hidden ">
           <Image
             src="/ERP/schoolerp.png"
             alt="EduPro School ERP"
             width={1400}
             height={700}
             className="w-full h-auto object-cover"
-            priority
           />
         </div>
       </div>
